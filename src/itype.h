eb6425676a4502a5aebefc12e6adb61a7bd28eb7
Hirmuolio <hirmuolio@gmail.com>
2022-08-06 22:01:26 +0300

    consume amount

--- src/itype.h
+++ src/itype.h
@@ -1366,9 +1366,11 @@ struct itype {
 
         int charges_to_use() const {
             if( tool ) {
-                return static_cast<int>( tool->charges_per_use );
+                return tool->charges_per_use;
+            } else if( comestible ) {
+                return 1;
             }
-            return 1;
+            return 0;
         }
 
         // for tools that sub another tool, but use a different ratio of charges
