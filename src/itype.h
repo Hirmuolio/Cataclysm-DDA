ae8cb399b0e5e5ae34e0eed2f88da03dec67de50
Hirmuolio <hirmuolio@gmail.com>
2022-08-09 14:23:19 +0300

    move to cpp

--- src/itype.h
+++ src/itype.h
@@ -1364,14 +1364,7 @@ struct itype {
 
         int charges_default() const;
 
-        int charges_to_use() const {
-            if( tool ) {
-                return tool->charges_per_use;
-            } else if( comestible ) {
-                return 1;
-            }
-            return 0;
-        }
+        int charges_to_use() const;
 
         // for tools that sub another tool, but use a different ratio of charges
         int charge_factor() const {
