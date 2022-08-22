fa2d8e14ab61affc305cdfbf2f7f2e251373d548
Hirmuolio <hirmuolio@gmail.com>
2022-08-22 12:02:57 +0300

    typo

--- src/suffer.cpp
+++ src/suffer.cpp
@@ -816,7 +816,7 @@ void suffer::in_sunlight( Character &you )
         const double sleeve_factor = you.armwear_factor();
         const bool has_hat = you.wearing_something_on( bodypart_id( "head" ) );
         const float weather_factor = std::max( incident_sun_irradiance( get_weather().weather_id,
-                                               calendar::turn ) / irradiance::normal, 1 );
+                                               calendar::turn ) / irradiance::moderate, 1.f );
         const int player_local_temp = units::to_fahrenheit( get_weather().get_temperature( position ) );
         const int flux = ( player_local_temp - 65 ) / 2;
         if( !has_hat ) {
